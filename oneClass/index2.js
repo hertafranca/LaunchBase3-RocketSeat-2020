a17ef75e471d554e9fd28a93e9e0e6dfc4b1610b
hertafranca <hertafranca.tech@gmail.com>
2020-02-26 17:07:05 -0300

    Strings and numbers.

---
+++ oneClass/index2.js
@@ -0,0 +1,15 @@
+// Number variable:
+
+const student1='Mayk'
+const  studentGrade1=9.8
+
+const student2='Diego'
+const studentGrade2=10.0
+
+const student3='Fulano'
+const studentGrade3=2
+
+const media= (studentGrade1 + studentGrade2+ studentGrade3)/ 3
+
+console.log(media)
+  
