87432984b75808a68bd7cadcaac0362bea1d6dc3
hertafranca <hertafranca.tech@gmail.com>
2020-02-26 17:22:47 -0300

    Conditional

---
+++ oneClass/index4.js
@@ -0,0 +1,20 @@
+//If and else condicional:
+
+const student1='Peter'
+const  studentGrade1=3.5
+
+const student2='John'
+const studentGrade2=4.5
+
+const student3='Paul'
+const studentGrade3=2
+
+const media= (studentGrade1 + studentGrade2+ studentGrade3)/ 3
+
+if (media > 5) {
+    console.log(`The media was ${media}.Congratulation`)
+} else {
+    console.log('The media is less that 5')
+}
+
+  
