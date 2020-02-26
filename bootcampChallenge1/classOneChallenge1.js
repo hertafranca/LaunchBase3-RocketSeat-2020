5e93045167c936b16b798144520a40a2d3665e27
hertafranca <hertafranca.tech@gmail.com>
2020-02-26 17:00:41 -0300

    Challenge : Calculating IMC.

---
+++ bootcampChallenge1/classOneChallenge1.js
@@ -0,0 +1,74 @@
+//Cálculo imc;
+
+const client1='Susan';
+const weight1=84;
+const height1=1.88;
+
+const imcClient1=weight1/(height1*height1)
+
+console.log(imcClient1)
+
+if (imcClient1 >= 30) {
+  console.log('You are overweight!')
+} else {
+  console.log('You are not overweight!')
+}
+
+const client2='Peter';
+const weight2=110;
+const height2=1.75;
+
+const imcClient2=weight2/(height2*height2)
+
+console.log(imcClient2)
+
+if (imcClient2 >= 60) {
+  console.log('You are overweight!')
+} else {
+  console.log('You are not overweight!')
+}
+
+const client3='John';
+const weight3=145;
+const height3=1.70;
+
+const imcClient3=weight3/(height3*height3)
+
+console.log(imcClient3)
+
+if (imcClient3 >= 60) {
+  console.log('You are overweight!')
+} else {
+  console.log('You are not overweight!')
+}
+
+const client4='Paul';
+const weight4=185;
+const height4=1.70;
+
+const imcClient4=weight4/(height4*height4)
+
+console.log(imcClient4)
+
+if (imcClient4 >= 40) {
+  console.log('You are overweight!')
+} else {
+  console.log('You are not overweight!')
+}
+
+const client5='Sarah';
+const weight5=120;
+const height5=1.50;
+
+const imcClient5=weight5/(height5*height5)
+
+console.log(imcClient5)
+
+if (imcClient5 >= 40) {
+  console.log('You are overweight!')
+} else {
+  console.log('You are not overweight!')
+}
+
+
+
