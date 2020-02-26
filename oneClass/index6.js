7e67b5bcfa4066b464ab29dfe6751c0cf4d6d84d
hertafranca <hertafranca.tech@gmail.com>
2020-02-26 17:23:54 -0300

    Logical Operators.

---
+++ oneClass/index6.js
@@ -0,0 +1,13 @@
+//Logical Operators:
+
+console.log(5==5 && 6==6)
+console.log(5==5 && 6==7)
+console.log(5==5 && 6!=6)
+
+console.log(5==5 || 6==6)
+console.log(5==5 || 6!=6)
+console.log(5==4 || 6!=6)
+
+console.log(!(5>6))
+console.log(!(5<6))
+console.log(!(5==5))
