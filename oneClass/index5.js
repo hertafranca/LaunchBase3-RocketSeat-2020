b5fee05b6af5ea192c6c6ae2ec87f76600f14092
hertafranca <hertafranca.tech@gmail.com>
2020-02-26 17:23:29 -0300

    Relative and Comparative operators.

---
+++ oneClass/index5.js
@@ -0,0 +1,11 @@
+//Relational and Comparative operators:
+console.log(5>4)
+console.log(5<4)
+console.log(5>=4)
+console.log(5<=4)
+console.log(4<=4)
+console.log(3<=4)
+console.log(4=="4")
+console.log(4==="4")
+console.log(4!="5")
+console.log(4!=="5")
