cb1621f1469b537d45d73179d3f9bc25da5de652
hertafranca <hertafranca.tech@gmail.com>
2020-02-27 12:45:29 -0300

    Practing with objects.

---
+++ oneClass/index8.js
@@ -0,0 +1,20 @@
+const student0 = {
+  name:'Mayk',
+  grade: 9.8
+}
+
+const student1 = {
+name:'Diego',
+grade:10
+}
+
+const student2 = {
+  name:'Gay',
+  grade:2
+}
+
+const media = [
+student0.grade + student1.grade +
+ student2.grade]/3
+
+console.log(student1)
