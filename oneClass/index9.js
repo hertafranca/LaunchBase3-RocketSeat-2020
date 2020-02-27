db3ea325fcb5024b9b9e08f74e4361eb1035b967
hertafranca <hertafranca.tech@gmail.com>
2020-02-27 12:46:07 -0300

    Practing with Arrays.

---
+++ oneClass/index9.js
@@ -0,0 +1,20 @@
+const students = [ 
+{
+  name:'Mayk',
+  grade: 9.8
+},
+{
+  name:'Diego',
+  grade:10
+},
+{
+  name:'Gay',
+  grade:2
+}
+]
+
+const nameStudents = ['Mayk', 'Diego', 'Gay']
+
+console.log(students)
+
+const media=(students[0].grade + students[1].grade + students[2].grade) / 3
